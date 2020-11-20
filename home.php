d20b90c9f798721b039632647b68e6fa64509081
Sakshi Bhandarkar <sakshi300699.b@gmail.com>
2020-11-21 04:25:19 +0530

    folder structure updated

--- home.php
+++ home.php
@@ -1,26 +1,40 @@
 <html>
 <head>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="./CSS/style.css">
   <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@600&display=swap" rel="stylesheet">
   
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
 </head>
-<body>
 
 <header id="header">
-
   <div id="brand">
-     <img id="header-img" src="./assests/icon.png" href="home.php"/>
-     
+     <img id="header-img" src="assets/icon.png" href="home.php"/>
   </div>
   <nav id="nav-bar">
     <a class="nav-link" href="#">Check Health Status</a>
     <a class="nav-link" href="#">Find Groups</a>
     <a class="nav-link" href="#">Connect to Doctor/Counsellor</a>
     <a class="nav-link" href="#">Login/Signup</a>
-    
   </nav>
 </header>
+<body>
 
+  <div id="health-tracker-card">
+    <h1 style="color:white; font-size:72px;">Health Tracker</h1>
+    <img src="assets/health-track-img.png" id="health-track-img">
+  </div>
+  <div>
+    <div style="display:flex; justify-content:center;">
+      <div id="sub-card">
+        <h1 style="color:white; margin:60px 15px 0px 15px;">Consult Doctor/ Counselor</h1>
+      </div>
+      <div id="sub-card">
+        <h1 style="color:white; margin:60px 15px 0px 15px;">Community Forum</h1>
+      </div>
+      <div id="sub-card">
+        <h1 style="color:white; margin:60px 15px 0px 15px;">Wellness Products</h1>
+      </div>
+    </div>
+  </div>
 </body>
 <html>
