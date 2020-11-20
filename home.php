c0e508007422e4261a6ecfcbd4095dc79fe1ebfd
Sakshi Bhandarkar <sakshi300699.b@gmail.com>
2020-11-21 03:59:05 +0530

    form added

--- home.php
+++ home.php
@@ -2,6 +2,7 @@
 <head>
   <link rel="stylesheet" href="style.css">
   <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@600&display=swap" rel="stylesheet">
+  
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
 </head>
 <body>
@@ -10,7 +11,7 @@
 
   <div id="brand">
      <img id="header-img" src="./assests/icon.png" href="home.php"/>
-     <a href="Home.php" style="text-decoration:none; color:#e8270b;"><h1 style="font-family: 'Dancing Script', cursive;"></h1></a>
+     
   </div>
   <nav id="nav-bar">
     <a class="nav-link" href="#">Check Health Status</a>
@@ -20,7 +21,6 @@
     
   </nav>
 </header>
-
 
 </body>
 <html>
