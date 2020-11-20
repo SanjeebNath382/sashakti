15885a7645bc47117fa6afebd1c7dab78cf389d4
Sakshi Bhandarkar <sakshi300699.b@gmail.com>
2020-11-21 04:45:12 +0530

    folder structure updated
    folder structure changed

---
+++ Products.php
@@ -0,0 +1,73 @@
+<html>
+<head>
+  <link rel="stylesheet" href="./CSS/product-style.css">
+</head>
+<body>
+    <div id="home-content">
+    <input type="text" id="search-product" placeholder="Search products"></input>
+    </div>
+    <div id="content-home">
+            <div id='product-results'>
+                <div class='product-card'>
+                <img src='images/p1.jpeg' class='product-img'></img>
+                    <a href='https://www.amazon.in/Reusable-Menstrual-Women-Medical-Silicone/dp/B07WZP3NPQ/ref=sr_1_3_sspa?dchild=1&keywords=menstrual+cup&qid=1605903686&sr=8-3-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFRRjJQV0I2QTNYSVEmZW5jcnlwdGVkSWQ9QTA5NjIzNzYzVE04T0xUT0tWTDQzJmVuY3J5cHRlZEFkSWQ9QTAwMjEwNDExMzdEVk9IV0Q4SkYmd2lkZ2V0TmFtZT1zcF9hdGYmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl' style='text-decoration:none;color:black;'>
+                    <div class='product-content'>
+                        <h1 class='product-name'>Menstrual Cup</h1>
+                        <h3 style="color:white;">Alternate menstrual hygiene product</h3>
+                    </div></a>
+                </div>
+            </div>
+            <div id="product-results">
+                <div class="product-card">
+                    <img src="images/p2.jpg" class="product-img"></img>
+                    <a href="https://www.amazon.in/dp/B00S6KDGNE/ref=cm_sw_r_wa_apa_i_9xcUFbD6ZA55T?_encoding=UTF8&psc=1" style="text-decoration:none;color:black;">
+                    <div class="product-content">
+                        <h1 class="product-name">Hygeine Wash</h1>
+                        <h3 style="color:white;">For personal hygiene</h3>
+                    </div></a>
+                </div>
+            </div>
+            <div id="product-results">
+                <div class="product-card">
+                    <img src="images/p3.jpg" class="product-img"></img>
+                    <a href="https://www.amazon.in/Siddhayu-Painquit-Natural-Ayurvedic-Muscular/dp/B08CH2X993" style="text-decoration:none;color:black;">
+                    <div class="product-content">
+                        <h1 class="product-name">Pain-relief roll on</h1>
+                        <h3 style="color:white;">Relief from cramps, backache,etc.</h3>
+                    </div></a>
+                </div>
+            </div>
+            <div id="product-results">
+                <div class="product-card">
+                    <img src="images/p4.jpg" class="product-img"></img>
+                    <a href="https://www.amazon.in/dp/B084ZKFR5Q/ref=cm_sw_r_wa_apa_i_HwcUFb9QB625J" style="text-decoration:none;color:black;">
+                    <div class="product-content">
+                        <h1 class="product-name">Multi-vitamin Supplement</h1>
+                        <h3 style="color:white;">For daily vitamin needs of women</h3>
+                    </div></a>
+                </div>
+            </div>
+            <div id="product-results">
+                <div class="product-card">
+                    <img src="images/p5.jpg" class="product-img"></img>
+                    <a href="https://www.amazon.in/dp/B08JVD64Q3/ref=cm_sw_r_wa_apa_i_LvcUFbJPB8DW7" style="text-decoration:none;color:black;">
+                    <div class="product-content">
+                        <h1 class="product-name">Yoga mat</h1>
+                        <h3 style="color:white;">Proper setup for yoga</h3>
+                    </div></a>
+                </div>
+            </div>
+            <div id="product-results">
+                <div class="product-card">
+                    <img src="images/p6.jpg" class="product-img"></img>
+                    <a href="https://www.amazon.in/HealthyHey-Supplement-Women-100-Chelated/dp/B083RZ69ZN/r" style="text-decoration:none;color:black;">
+                    <div class="product-content">
+                        <h1 class="product-name">Iron supplement</h1>
+                        <h3 style="color:white;">For supplementing iron levels in blood</h3>
+                    </div></a>
+                </div>
+            </div>
+            
+    </div>
+</body>
+</html>
