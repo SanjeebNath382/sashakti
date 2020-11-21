d9574355beeaa0d000e639b7e845408d65e6a16a
Sakshi Bhandarkar <sakshi300699.b@gmail.com>
2020-11-21 23:05:44 +0530

    final files added

---
+++ FindGroup.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Community Engagement Activities</title>
+  </head>
+  <body>
+  <h1 style="margin-left:35%;margin-top:5%;color:#ff3256;font-family: fantasy;"><b>Community Engagement Activities</b></h1><br>
+  <div class='container' style='background:linear-gradient(to bottom, #ffff00 0%, #33cc33 100%);text-align:center;width:800px;margin: 30px auto; border-radius: 15px; box-shadow: 0px 2px 2px #ff3256;border: 2px solid;overflow:none'>
+            <h2 style='font-family:Arial, Helvetica, sans-serif;'><b>Art of Living Session</b></h2>
+            <p><b>Location</b></p>
+            <p style="color:white;">Bhilai Center, Bhilai, Chhattisgarh, India</p>
+      <p><b>  Organizer</b></p>
+      <p style="color:white">Sudarshan Kriya Practice Groups in Bhilai</p>
+      <p><b>  Description</b></p>
+      <p style="color:white">Join The Online Meditation &
+Breath Workshop. Learn Yoga and Meditation</p>
+
+            </div>  <br>
+
+            <div class='container' style='background:linear-gradient(to bottom, #ffff00 0%, #33cc33 100%);text-align:center;width:800px;margin: 30px auto; border-radius: 15px; box-shadow: 0px 2px 2px #ff3256;border: 2px solid;overflow:none'>
+            <h2 style='font-family:Arial, Helvetica, sans-serif;'><b>Training on Mental Health First Aid</b></h2>
+            <p><b>Location</b></p>
+            <p style="color:white;">The Ruby, Bombay Chamber, 4th Floor, NW 29 Senapati Bapat Marg, Dadar (West), Mumbai 400028</p>
+      <p><b>  Organizer</b></p>
+      <p style="color:white">Bombay Chamber of Commerce and Industry</p>
+      <p><b>  Description</b></p>
+      <p style="color:white">Build awareness or enhance general awareness of Signs & symptoms of mental illness and how to identify potential people in workplace to do ‘triage’ – motivate and enable reach out to professional mental health experts.</p>
+
+            </div>
+  </body>
+</html>
