528b1469b8167f8f9c05b02bec2b2a900e2d135c
Sakshi Bhandarkar <sakshi300699.b@gmail.com>
2020-11-21 16:07:54 +0530

    added files

---
+++ profile.php
@@ -0,0 +1,58 @@
+<html>
+<head>
+<br>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+.card {
+  box-shadow: 0 2px 8px 0 rgba(5, 3, 3, 0.6);
+  max-width: 300px;
+  margin: auto;
+  text-align: center;
+  font-family: arial;
+}
+
+.title {
+  color: grey;
+  font-size: 18px;
+}
+
+button {
+  border: none;
+  outline: 0;
+  display: inline-block;
+  padding: 8px;
+  color: white;
+  background-color: turquoise;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+  font-size: 18px;
+}
+
+a {
+  text-decoration: none;
+  font-size: 22px;
+  color: black;
+}
+
+button:hover, a:hover {
+  opacity: 0.7;
+}
+</style>
+</head>
+<body>
+
+
+<div class="card">
+ <!-- <img src="person-icon-female-user-profile-avatar-vector-18833553.jpg" alt="User" style="width:100%"> -->
+  <br>
+  <h2>Sakshi Bhandarkar</h2>
+  <p class="title">Patient</p>
+  <p>Email- sakshi@gmail.com</p>
+  <div style="margin: 24px 0;">
+  </div>
+  <p><button >Contact</button></p>
+</div>
+
+</body>
+</html>
